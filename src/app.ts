65dd623df9cf374abd41efb736367a3dddbcc0eb
ShohjahonAhmad <shox_0502@mail.ru>
2025-10-23 14:06:46 +0200

    cors added

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import authRouter from "./routes/auth.js"
 import dotenv from 'dotenv';
 import authenticated from './middleware/authenticated.js';
+import cors from 'cors'
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hello World!")
